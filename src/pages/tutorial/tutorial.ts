aaad458371bd19daa6887d7cbda62c47641bf71a
Nkosi <nkosinolwazin@nvisionit.co.za>
2018-05-24 08:25:27 +0200

    Name changes

--- src/pages/tutorial/tutorial.ts
+++ src/pages/tutorial/tutorial.ts
@@ -34,18 +34,18 @@ export class TutorialPage {
       console.log('Loaded values', values);
       this.slides = [
         {
-          title: 'Welcome to Service-24-6!',
-          description: 'Service-24-6 helps you discover service providers.',
+          title: 'Welcome to MY CAR WASH!',
+          description: 'MY CAR WASH helps you discover mobile car wash services around you.',
           image: 'assets/img/ica-slidebox-img-1.png',
         },
         {
           title: 'Search and Discover Service providers around you!' ,
-          description: 'Service-24-6 uses informative maps to inform you of service providers around',
+          description: 'MY CAR WASH uses informative maps to inform you of mobile car wash services around',
           image: 'assets/img/ica-slidebox-img-2.png',
         },
         {
-          title: 'Service-24-6 connects you',
-          description: 'This is one of the best apps in the world in the service industry',
+          title: 'MY CAR WASH connects you',
+          description: 'This is one of the best apps in the world in the car wash industry',
           image: 'assets/img/ica-slidebox-img-3.png',
         }
       ];
